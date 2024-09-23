bb04d5989f1517642d5da314c569db8bc658ce44
kulinski-cmu <kulinski@andrew.cmu.edu>
2024-09-23 09:56:46 -0400

    added comments to the JavaScript

--- solution-hw3/JavaScript/productDetailSelection.js
+++ solution-hw3/JavaScript/productDetailSelection.js
@@ -4,17 +4,27 @@ let selectGlaze = document.querySelector('#flavors');
 let selectSize = document.querySelector('#amount');
 let glazeValue = 'Original';
 let sizeValue = 1;
-
+/*
+*This function computes the new price based on the selections made by the user
+*param --> glazingPrice : price associated with the glaze; packPrice : price assocated with the number of rolls
+*/
 function computePrice(glazingPrice, packPrice) {
     const price = (( basePrice + glazingPrice) * packPrice).toFixed(2);
     displayPrice(price);
 }
 
+/*
+* This function displays the new price on the webpage
+*param --> price : computed price of the rolls
+ */
 function displayPrice(price) {
     let priceElement = document.querySelector('#product-price');
     priceElement.innerText = price;
 }
 
+/*
+ * This function runs when the glaze selector is changed and finds the correct values for the price function 
+ */
 function onSelectGlazeChange(){
     glazeValue = this.value;
     for(let i = 0; i < 4; i++){
@@ -28,6 +38,9 @@ function onSelectGlazeChange(){
     }
 }
 
+/*
+* This function runs when the size selector is changed and finds the correct values for the price function
+ */
 function onSelectSizeChange(){
     sizeValue = parseInt(this.value);
     for ( i = 0; i < 4; i++){
@@ -41,14 +54,16 @@ function onSelectSizeChange(){
     }
 }
 
+//objects that contain all the information required for the two selectors
 const glazing = {
-    option: [['Original', 0.00], ['Sugar milk', 0.00], ['Vanilla milk', 0.50], ['Double chocolate', 1.50]],
+    option: [['Keep original', 0.00], ['Sugar milk', 0.00], ['Vanilla milk', 0.50], ['Double chocolate', 1.50]],
 };
 
 const packSize = {
     option: [[1, 1.00], [3, 3.00], [6, 5.00], [12, 10.00]],
 };
 
+//event listeners to call the change functions
 selectGlaze.addEventListener('change', onSelectGlazeChange);
 selectSize.addEventListener('change', onSelectSizeChange);
 
