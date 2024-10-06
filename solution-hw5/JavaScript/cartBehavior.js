d70d159099ca6ae79dcdfae3b2b76935654c65fa
kulinski-cmu <kulinski@andrew.cmu.edu>
2024-10-06 17:38:01 -0400

    updated cart page for template

--- solution-hw5/JavaScript/cartBehavior.js
+++ solution-hw5/JavaScript/cartBehavior.js
@@ -1,3 +1,7 @@
+//let template = document.querySelector('#product-template');
+//let templateContent = template.content;
+//const productList = document.querySelector('#product-list');
+//productList.prepend(templateContent);
 
 const cart = [];
 
@@ -15,25 +19,71 @@ const sizes = {
     "12" : 10.00,
 };
 
+const images = {
+    'Original' : 'original-cinnamon-roll.jpg',
+    'Walnut' : 'walnut-cinnamon-roll.jpg',
+    'Raisin' : 'raisin-cinnamon-roll.jpg',
+    'Apple' : 'apple-cinnamon-roll.jpg',
+};
+
 class Roll{
     constructor(rollType, rollGlazing, packSize, rollPrice) {
         this.type = rollType;
         this.glazing = rollGlazing;
         this.size = packSize;
         this.basePrice = rollPrice;
+
+        this.element = null;
     }
 }
 
-const originalRoll = new Roll('Original','Sugar Milk', 1, 2.49);
-const walnutRoll = new Roll('Walnut', 'Vanilla Milk', 12, 3.49);
-const raisinRoll = new Roll('Raisin', 'Sugar Milk', 3, 2.99);
-const appleRoll = new Roll('Apple', 'Original', 3, 3.49);
+const originalRoll = new Roll("Original","Sugar Milk", "1", 2.49);
+const walnutRoll = new Roll("Walnut", "Vanilla Milk", "12", 3.49);
+const raisinRoll = new Roll("Raisin", "Sugar Milk", "3", 2.99);
+const appleRoll = new Roll("Apple", "Original", "3", 3.49);
 
 cart[0] = originalRoll;
 cart[1] = walnutRoll;
 cart[2] = raisinRoll;
 cart[3] = appleRoll;
 
-function computePrice(glazePrice, packPrice, basePrice){
-    const price = ((basePrice + glazePrice) * packPrice).toFixed(2);
+function computePrice(roll){
+    let glazePrice = glazes[roll.glazing];
+    let packPrice = sizes[roll.size];
+    const price = ((roll.basePrice + glazePrice) * packPrice).toFixed(2);
+    return price;
 }
+
+function createRoll(roll){
+    const template = document.querySelector('#product-template');
+    const clone = template.content.cloneNode(true);
+    roll.element = clone.querySelector('.roll-element');
+
+    const productList = document.querySelector('#product-list');
+    productList.prepend(roll.element);
+   let picture = roll.element.querySelector('#image');
+    let name = roll.element.querySelector('#name');
+    let glazeType = roll.element.querySelector('#glaze');
+    let price = roll.element.querySelector('#pack-price');
+    let numOfRolls = roll.element.querySelector('#pack-size');
+    picture.src =  '../assets/products/' + images[roll.type];
+    name.innerText = roll.type;
+    glazeType.innerText = 'Glazing: ' + roll.glazing;
+    price.innerText = '$' + computePrice(roll);
+    numOfRolls.innerText = 'Pack Size: ' + roll.size;
+}
+
+for(let i = cart.length-1; i >= 0; i--){
+    createRoll(cart[i]);
+}
+
+let totalPrice = 0;
+function computeTotal(){
+    for(let i = 0; i < cart.length; i++){
+        totalPrice = totalPrice + parseInt(computePrice(cart[i]));
+    }
+    return totalPrice;
+}
+
+const totalPriceElement = document.querySelector('#total-price');
+totalPriceElement.innerText = '$' + computeTotal();
