c5b03de451298f92ea626e36199e66c84124e751
kulinski-cmu <kulinski@andrew.cmu.edu>
2024-11-20 22:39:34 -0500

    added templates and fixed popup

--- final-project/pattern-make-behavior.js
+++ final-project/pattern-make-behavior.js
@@ -1,46 +1,68 @@
 import threadColorsByName from './threadColors.js';
 
-
-
 class Thread{
-    constructor(name, hex, number) {
+    constructor(name) {
         this.name = name;
-        this.hex = hex;
-        this.number = number;
+        //this.hex = hex;
+        //this.number = number;
         
         this.element = null;
     }
 }
 
 let addNewColorButton = document.querySelector('#new-color-button');
+const colors = []; 
+let currentColor = 'test';
+const userInput = 'Cocoa - Very Dark';
 
-const myPopup = new Popup ({
+
+
+const popup = new Popup ({
     id : "color-add",
     title : "Choose a New Color to Add",
     content : `
-    {btn-enter}[enter a color name]`,
+     {btn-search}[Search]{btn-enter}[Add]`,
+
+    
+
+    loadCallback: () => {
+        document.querySelector(".popup.color-add button.enter").addEventListener("click", () => {
+            colors.push(currentColor);
+            console.log(colors);
+        });
+        
+        document.querySelector(".popup.color-add button.search").addEventListener("click", () => {
+            console.log(threadColorsByName[userInput].number);
+       });
+    },
 });
 
+
 function onClickAddNewColor(){
-    myPopup.show();
+    popup.show();
 }
 
-const colors = []; 
+
 
 function createColor(thread) {
-    const template = document.querySelector('.color-option');
-    const clone = template.textContent.cloneNode(true);
-    thread.element = clone.querySelector('.thread-element');
+    const template = document.querySelector('#color-template');
+    const clone = template.content.cloneNode(true);
+    thread.element = clone.querySelector('.color-option');
 
-    const threadList = document.querySelector('.thread-list');
-    threadList.prepend(thred.element);
+    const threadList = document.querySelector('.color-list');
+    threadList.append(thread.element);
     let name = thread.element.querySelector('#name');
     let number = thread.element.querySelector('#number');
-    let color = thread.element.querySelector('#color-square');
+    let color = thread.element.querySelector('.color-square');
 
-        name.innerText = colors[i];
-        number.innerText =threadColorsByName[colors[i]].number;
-        color.style.color = threadColorsByName[colors[i]].hex;
+        name.innerText = thread.name;
+        number.innerText =threadColorsByName[thread.name].number;
+        color.style.backgroundColor = threadColorsByName[thread.name].hex;
 }
 
+
 addNewColorButton.addEventListener('click', onClickAddNewColor);
+colors[0] = new Thread('Cocoa - Very Dark');
+colors[1] = new Thread("Tender Green - Very Light");
+createColor(colors[0]);
+createColor(colors[1]);
