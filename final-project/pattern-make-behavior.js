84b16c8ce64f8317d962a7c658c4689dec0b41d3
kulinski-cmu <kulinski@andrew.cmu.edu>
2024-11-30 21:37:11 -0500

    added a template to make the rows

--- final-project/pattern-make-behavior.js
+++ final-project/pattern-make-behavior.js
@@ -14,6 +14,12 @@ class Square{
     }
 }
 
+class Div{
+    constructor(){
+        this.element = null;
+    }
+}
+
 let addNewColorButton = document.querySelector('#new-color-button');
 
 const colors = []; 
@@ -67,7 +73,7 @@ function createColor(thread) {
     });
 }
 
-function createGridRows(){
+function createGridCols(){
     const template = document.querySelector('#pattern-template');
     const clone = template.content.cloneNode(true);
     const square = new Square();
@@ -79,6 +85,15 @@ function createGridRows(){
     square.element.addEventListener('click', () => {
         square.element.style.backgroundColor = threadColorsByName[currentColor.innerText].hex;
     });
+}
+
+function createGridRows(){
+    const template = document.querySelector('#row-template');
+    const clone = template.content.cloneNode(true);
+    const div = new Div();
+    div.element = clone.querySelector('#square-div');
+
+    document.querySelector('#right-of-page').append(div.element);
 }
 
 let clickIncreaseRows = document.querySelector('#upRows');
@@ -93,7 +108,6 @@ function onChangeRowsUp(){
     let rowsNum = parseInt(rowsAmount.innerText);
     rowsNum++;
     rowsAmount.innerText =rowsNum;
-    createGridRows();
 }
 
 function onChangeRowsDown(){
@@ -106,6 +120,7 @@ function onChangeColsUp(){
     let colsNum = parseInt(colsAmount.innerText);
     colsNum++;
     colsAmount.innerText =colsNum;
+    createGridCols();
 }
 
 function onChangeColsDown(){
@@ -124,9 +139,11 @@ clickDecreaseCols.addEventListener('click', onChangeColsDown);
 
 colors[0] = new Thread('Cocoa - Very Dark');
 colors[1] = new Thread("Tender Green - Very Light");
-//const square = new Square();
 for(let i = 0; i < parseInt(rowsAmount.innerText); i++){
-    createGridRows(); 
+    createGridRows();
+}
+for(let i = 0; i < parseInt(colsAmount.innerText); i++){
+    createGridCols(); 
 }
 createColor(colors[0]);
 createColor(colors[1]);
