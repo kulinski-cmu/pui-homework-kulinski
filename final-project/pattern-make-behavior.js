316afe90717b9736b7256a99ddd3e906a68b6537
kulinski-cmu <kulinski@andrew.cmu.edu>
2024-12-09 14:20:21 -0500

    empty

--- final-project/pattern-make-behavior.js
+++ final-project/pattern-make-behavior.js
@@ -2,6 +2,7 @@ import threadColorsByName from './threadColors.js';
 import Swal from './node_modules/sweetalert2/src/sweetalert2.js';
 import autoComplete from "./node_modules/@tarekraafat/autocomplete.js/src/autoComplete.js";
 
+
 class Thread{
     constructor(name) {
         this.name = name;
